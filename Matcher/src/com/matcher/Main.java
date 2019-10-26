10d334e8a5a0da93364b71d55df8e645ef0d2136
malinowska423 <amalinowska423@gmail.com>
2019-10-26 12:35:29 +0200

    main loop implemented

--- Matcher/src/com/matcher/Main.java
+++ Matcher/src/com/matcher/Main.java
@@ -1,8 +1,36 @@
 package com.matcher;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        if (args.length != 2 ||
+            !(args[0].equals("-fa") || args[0].equals("-kmp"))) {
+            System.err.println("Invalid arguments." +
+                " Please type \"-fa pattern\" " +
+                "\n\tfor finite automaton algorithm " +
+                "\nor \"-kmp pattern\" " +
+                "\n\tfor Knuth-Morris-Pratt algorithm." );
+            
+        } else {
+            System.out.println("Type -q to exit");
+            Scanner scanner = new Scanner(System.in);
+            Matcher matcher;
+            if (args[0].equals("-fa")) {
+                matcher = new FiniteAutomaton(args[1]);
+            } else {
+                matcher = new KMP(args[1]);
+            }
+            System.out.println("You searching with " + matcher.getClass().getSimpleName());
+            System.out.println("Enter the line of text to search: ");
+            String input = scanner.nextLine();
+            while (!input.equals("-q") && !input.equals("-Q")) {
+                System.out.println("Pattern occurs with offset "
+                    + matcher.findMatch(input));
+                System.out.println("Enter the line of text to search: ");
+                input = scanner.nextLine();
+            }
+        }
     }
 }
