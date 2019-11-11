3de9e7d4bc379b0f773a5a6cfa3435913f14ef2b
malinowska423 <amalinowska423@gmail.com>
2019-11-11 21:00:56 +0100

    flex all tasks first attempt

---
+++ Flex/zad3/wynikdoc.cpp
@@ -0,0 +1,37 @@
+#include "test/*asdf*/header.h"
+#include <stdio.h>
+#include <foobaz.h>
+
+/** \brief Java style Doc String - Foo function 
+*/
+int foo();
+
+int bar(); /**< Bar function 
+*/
+
+/// .NET Style Doc Strin
+int g_global_var = 1;
+
+
+int baz();
+
+
+
+volatile int g_global;
+
+
+int main(int argc, const char* argv)
+{
+    printf("/* foo bar");
+    
+
+    
+    baz();
+    
+
+    
+    baz();
+
+
+    return 1;
+}
